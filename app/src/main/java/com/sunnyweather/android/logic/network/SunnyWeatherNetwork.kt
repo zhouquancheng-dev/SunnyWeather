24699d38e749ae256dba6169270ace632b8a7e06
zhou <1627047368@qq.com>
2021-04-07 01:41:18 +0800

    实现网络层的初始化工作 0.1

---
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -0,0 +1,40 @@
+package com.sunnyweather.android.logic.network
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+
+object SunnyWeatherNetwork {
+
+    //使用ServiceCreator创建了一个PlaceService接口的动态代理对象，然后定义了一个searchPlaces()函数
+    private val placeService = ServiceCreator.create<PlaceService>()
+
+    //定义了一个searchPlaces()函数，并在这里调用刚刚在PlaceService接口中定义的searchPlaces()方法，以发起搜索城市数据请求
+    suspend fun searchPlaces(query: String) = placeService.searchPlaces(query)
+
+    private suspend fun <T> Call<T>.await(): T {
+        return suspendCoroutine { continuation ->
+            enqueue(object : Callback<T> {
+                override fun onResponse(call: Call<T>, response: Response<T>) {
+                    val body = response.body()
+                    if (body != null) {
+                        continuation.resume(body)
+                    } else {
+                        continuation.resumeWithException(RuntimeException("响应的主体为空"))
+                    }
+                }
+
+                override fun onFailure(call: Call<T>, t: Throwable) {
+                    continuation.resumeWithException(t)
+                }
+
+            })
+
+        }
+    }
+
+}
